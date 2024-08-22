8455ee024952e818562b92090d517a98d568e33c
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-22 17:17:31 +0300

    Add Task model, serializer, viewSet in LQ_Tasks

--- LQ_Tasks/models.py
+++ LQ_Tasks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    # Priority levels with game analogs
+    PRIORITY_CHOICES = [
+        (1, 'F'),
+        (2, 'E'),
+        (3, 'D'),
+        (4, 'C'),
+        (5, 'B'),
+        (6, 'A'),
+        (7, 'S'),
+        (8, 'SS'),
+        (9, 'SSS'),
+        (10, 'EX'),
+    ]
+
+    # Task status
+    STATUS_CHOICES = [
+        ('CREATED', 'Created'),
+        ('SCHEDULED', 'Scheduled'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('PROCESSING', 'Processing'),
+        ('ON_HOLD', 'On Hold'),
+        ('DEFERRED', 'Deferred'),
+        ('UNDER_REVIEW', 'Under Review'),
+        ('COMPLETED', 'Completed'),
+        ('FAILED', 'Failed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='CREATED')
+    deadline = models.DateTimeField(blank=True, null=True)
--- LQ_Tasks/views.py
+++ LQ_Tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+    @action(detail=False, methods=['get'])
+    def high_priority(self, request):
+        high_priority_tasks = self.queryset.filter(priority__gte=7)
+        serializer = self.get_serializer(high_priority_tasks, many=True)
+        return Response(serializer.data)
