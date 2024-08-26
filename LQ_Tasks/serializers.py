b0e3516a5f5d875fd9b65b45dbe3fba5a8aa916a
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-26 04:46:38 +0300

    Add Serializer for SubTask model

--- LQ_Tasks/serializers.py
+++ LQ_Tasks/serializers.py
@@ -1,16 +1,30 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, SubTask
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class SubTaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'priority', 'status', 'deadline']
-    
+        model = SubTask
+        fields = '__all__'
 
     def create(self, validated_data):
         request = self.context.get('request', None)
         if request and hasattr(request, 'user'):
             validated_data['user'] = request.user
         return super().create(validated_data)
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    subtasks = SubTaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'priority', 'status', 'subtasks']
+    
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        if request and hasattr(request, 'user'):
+            validated_data['user'] = request.user
+        return super().create(validated_data)
+
         
