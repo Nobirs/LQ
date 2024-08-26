9a61cbaeef20e3652cb43663661546378c7d3315
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-26 04:50:50 +0300

    add tests to check subtask API

--- LQ_Tasks/tests.py
+++ LQ_Tasks/tests.py
@@ -1,8 +1,9 @@
 # accounts/tests.py (или tasks/tests.py, в зависимости от структуры проекта)
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from accounts.models import CustomUser
-from .models import Task
+from .models import SubTask, Task
 
 class TaskTests(APITestCase):
     def setUp(self):
@@ -71,3 +72,67 @@ class TaskTests(APITestCase):
         # Проверяем, что задача удалена
         self.assertEqual(Task.objects.count(), 0)
 
+
+
+class SubTaskTests(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+        self.task = Task.objects.create(title='Main Task', priority=5, user=self.user)
+
+
+    def test_create_subtask(self):
+        url = reverse('subtask-list', kwargs={'task_pk': self.task.id})
+        data = {
+                    'title': 'Subtask 1',
+                    'description': 'Description for subtask 1',
+                    'priority': 4,
+                    'status': 'CREATED',
+                    'task': self.task.id
+                }
+        response = self.client.post(url, data, format='json')
+# {'view': <LQ_Tasks.views.TaskViewSet object at 0x7ca9d7ba80e0>, 'args': (), 'kwargs': {'pk': '1'}, 'request': <rest_framework.request.Request: POST '/api/tasks/1/subtasks/'>, 'response': <Response status_code=405, "application/json">}
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SubTask.objects.count(), 1)
+        self.assertEqual(SubTask.objects.get().title, 'Subtask 1')
+
+
+    def test_get_subtasks(self):
+        subtask1 = SubTask.objects.create(task=self.task, title='SubTask 1')
+        subtask2 = SubTask.objects.create(task=self.task, title='SubTask 2')
+
+        url = reverse('task-subtasks', args=[self.task.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['title'], subtask1.title)
+        self.assertEqual(response.data[1]['title'], subtask2.title)
+
+
+    def test_update_subtask(self):
+        subtask = SubTask.objects.create(task=self.task, title='Old SubTask')
+        url = reverse('subtask-detail', args=[self.task.id, subtask.id])
+        data = {'title': 'Updated SubTask'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(SubTask.objects.get().title, 'Updated SubTask')
+
+
+    def test_delete_subtask(self):
+        # Создаем подзадачу
+        subtask = SubTask.objects.create(task=self.task, title='SubTask to delete')
+        url = reverse('subtask-detail', args=[self.task.id, subtask.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(SubTask.objects.count(), 0)
+
+    def test_subtask_belongs_to_task(self):
+        # Создаем подзадачу
+        subtask = SubTask.objects.create(task=self.task, title='SubTask 1')
+
+        self.assertEqual(subtask.task, self.task)
