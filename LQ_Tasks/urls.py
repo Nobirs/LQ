774d5488670310f730b8b35d56fb088cfb79dabe
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-26 04:49:31 +0300

    Update api urls for subtask

--- LQ_Tasks/urls.py
+++ LQ_Tasks/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from rest_framework_nested import routers as nested_routers
+from .views import TaskViewSet, SubTaskViewSet
 
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
+tasks_router = nested_routers.NestedSimpleRouter(router, r'tasks', lookup='task')
+tasks_router.register(r'subtasks', SubTaskViewSet, basename='subtask')
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(tasks_router.urls)),
 ]
