0fed017426f783ae4fe800f0fe85151e91c128be
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-30 11:27:26 +0300

    Customized admin panel for CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email')
+class CustomUserAdmin(UserAdmin):
+    # Поля - в списке пользователей
+    list_display = ('username', 'email', 'first_name', 'last_name', 'gender', 'is_staff', 'level', 'points')
+
+    # - можно редактировать прямо из списка
+    list_editable = ('level', 'points', 'email')
+
+    # - по которым можно искать пользователей
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+
+    # - отображаемые при просмотре/редактировании пользователя
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Game Data', {'fields': ('points', 'level')}),
+    )
 
 
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
