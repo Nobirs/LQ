3ee024bae7574fbafafa665802d120bfe6434b9a
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-08-30 12:06:25 +0300

    Add Task, SubTask to admin panel

--- LQ_Tasks/admin.py
+++ LQ_Tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Task, SubTask
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'status', 'deadline', 'user')
+    list_filter = ('status', 'priority', 'deadline')
+    search_fields = ('title', 'description')
+    ordering = ('-deadline',)
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'status', 'deadline', 'task')
+    list_filter = ('status', 'priority', 'deadline')
+    search_fields = ('title', 'description')
+    ordering = ('-deadline',)
