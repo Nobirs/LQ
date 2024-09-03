bc80baf89f14cab3d87720d16ad63799ff544cb8
Alexandr Rohozhin <AlexNobirs13@gmail.com>
2024-09-04 00:04:17 +0300

    Add Note model with GenericRelation to Task and SubTask models
    - Created Note model with fields content_type, object_id, content_object, title, content, created_at, and updated_at.
    - Added GenericRelation for notes in Task and SubTask models.

--- LQ_Tasks/models.py
+++ LQ_Tasks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from accounts.models import CustomUser
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
 
 class Task(models.Model):
@@ -36,7 +38,7 @@ class Task(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='CREATED')
     deadline = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tasks')
-
+    notes = GenericRelation('Note')
 
     def __str__(self):
         return self.title
@@ -49,6 +51,7 @@ class SubTask(models.Model):
     status = models.CharField(max_length=20, choices=Task.STATUS_CHOICES, default='CREATED')
     deadline = models.DateTimeField(blank=True, null=True)
     task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+    notes = GenericRelation('Note')
 
     def __str__(self):
         return f'{self.task} ->  {self.title}'
@@ -61,3 +64,16 @@ class SubTask(models.Model):
         else:
             self.priority = self.task.priority
         super(SubTask, self).save(*args, **kwargs)
+
+
+class Note(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.content_object} -> {self.title}'
